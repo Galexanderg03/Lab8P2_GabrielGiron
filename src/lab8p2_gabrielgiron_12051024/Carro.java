6400646031df96c764738806036315cd330a301f
Galexanderg03 <galexanderg03@gmail.com>
2022-03-11 14:03:12 -0600

    Third Commit

---
+++ src/lab8p2_gabrielgiron_12051024/Carro.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8p2_gabrielgiron_12051024;
+
+import java.awt.Color;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Galex
+ */
+public class Carro implements Serializable{
+    private int NumID;
+    private String Corredor;
+    private Color color;
+
+    public Carro(int NumID, String Corredor, Color color) {
+        this.NumID = NumID;
+        this.Corredor = Corredor;
+        this.color = color;
+    }
+
+    public int getNumID() {
+        return NumID;
+    }
+
+    public void setNumID(int NumID) {
+        this.NumID = NumID;
+    }
+
+    public String getCorredor() {
+        return Corredor;
+    }
+
+    public void setCorredor(String Corredor) {
+        this.Corredor = Corredor;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return NumID+"";
+    }
+    
+    
+}
---
+++ src/lab8p2_gabrielgiron_12051024/McQueen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8p2_gabrielgiron_12051024;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Galex
+ */
+public class McQueen extends Carro{
+
+    public McQueen(int NumID, String Corredor, Color color) {
+        super(NumID, Corredor, color);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+}
